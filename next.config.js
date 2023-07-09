28e1c8156dfc69feeceee12d64ed6d24a4e09763
timpriadko <timpriadko@gmail.com>
2023-07-09 23:34:17 +0300

    update next version, next.config

--- next.config.js
+++ next.config.js
@@ -11,13 +11,11 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
